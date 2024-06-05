95028a0f4dc5bdb39f059852b42a4ba24685e0c8
Cristofer-Vargas <vargascristofer97@gmail.com>
2024-06-05 12:38:18 -0300

    Removendo tudo

--- CSharpMusic/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CSharpMusic.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
